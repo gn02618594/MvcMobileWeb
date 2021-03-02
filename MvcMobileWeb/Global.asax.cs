0a078c2a4a4b98466d2519d038764d0efb943019
gn02618594 <gn02618594@gmail.com>
2021-03-02 21:07:57 +0800

    新增 Display Modes 設定以支援 iPhone 及 Android 專用檢視頁

--- MvcMobileWeb/Global.asax.cs
+++ MvcMobileWeb/Global.asax.cs
@@ -5,13 +5,23 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Web.WebPages;
 
 namespace MvcMobileWeb
 {
     public class MvcApplication : System.Web.HttpApplication
     {
+        
         protected void Application_Start()
         {
+            //¥Nªí xxx.iPhone.cshtml ÀËµøÀÉ
+            DisplayModeProvider.Instance.Modes.Insert(0, new DefaultDisplayMode("iPhone") { 
+            ContextCondition=(context => context.GetOverriddenUserAgent().IndexOf("iPhone",StringComparison.OrdinalIgnoreCase) >= 0)
+            });
+            DisplayModeProvider.Instance.Modes.Insert(1, new DefaultDisplayMode("Android")
+            {
+                ContextCondition=(context => context.GetOverriddenUserAgent().IndexOf("Android",StringComparison.OrdinalIgnoreCase) >= 0)
+            });
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
